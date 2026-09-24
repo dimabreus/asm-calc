8e80e95ca4120a02db2233891703699cab511d19
Dmitriy <dmitriybreus5@gmail.com>
2026-09-24 04:53:25 +0300

    parser, interpreter & format ast

--- calc.c
+++ calc.c
@@ -1,16 +1,25 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define LEXEME_PLUS 0
-#define LEXEME_MINUS 1
-#define LEXEME_STAR 2
-#define LEXEME_SLASH 3
-#define LEXEME_LEFT_BRACKET 4
-#define LEXEME_RIGHT_BRACKET 5
-#define LEXEME_NUMBER 6
+#define TOKEN_PLUS 0
+#define TOKEN_MINUS 1
+#define TOKEN_STAR 2
+#define TOKEN_SLASH 3
+#define TOKEN_LEFT_BRACKET 4
+#define TOKEN_RIGHT_BRACKET 5
+#define TOKEN_NUMBER 6
+#define TOKEN_EOF 7
+
+#define NODE_LITERAL 0
+#define NODE_BINOP 1
+#define BINOP_PLUS 0
+#define BINOP_MINUS 1
+#define BINOP_MULTIPLY 2
+#define BINOP_DIVIDE 3
 
 typedef struct {
     int64_t *data;
@@ -33,23 +42,22 @@ void vecPop(Vector *vector) {
     vector->data[--vector->size] = 0;
 }
 
-void printVector(Vector *vector) {
-    printf("So this is my vector, size: %zu, cap: %zu\n", vector->size, vector->capacity);
-    for (int i = 0; i < vector->size; i++) {
-        printf("%d. %zu\n", i, vector->data[i]);
-    }
+void vecClear(Vector *vector) {
+    memset(vector->data, 0, vector->size * sizeof(*vector->data));
+    vector->size = 0;
 }
 
-Vector lexemes = {0};
+Vector tokens = {0};
 int lexerCurrent = 0;
 
-void lexerAddLexeme(int8_t type, int64_t literal) {
-    int8_t *lexeme = malloc(1 + 8);
+void lexerAddToken(int8_t type, int64_t literal) {
+    int8_t *token = malloc(1 + 8);
+    memset(token, 0, 1 + 8);
 
-    lexeme[0] = (uint8_t)type;
-    memcpy(lexeme + 1, &literal, sizeof(int64_t));
+    token[0] = (uint8_t)type;
+    memcpy(token + 1, &literal, sizeof(int64_t));
 
-    vecAppend(&lexemes, (int64_t)lexeme);
+    vecAppend(&tokens, (int64_t)token);
 }
 
 void lexer(char *code) {
@@ -57,6 +65,7 @@ void lexer(char *code) {
         char c = code[lexerCurrent];
 
         if (c == '\0') {
+            lexerAddToken(TOKEN_EOF, 0);
             break;
         }
 
@@ -65,12 +74,12 @@ void lexer(char *code) {
             continue;
         }
 
-        if (c == '+') lexerAddLexeme(LEXEME_PLUS, 0);
-        else if (c == '-') lexerAddLexeme(LEXEME_MINUS, 0);
-        else if (c == '*') lexerAddLexeme(LEXEME_STAR, 0);
-        else if (c == '/') lexerAddLexeme(LEXEME_SLASH, 0);
-        else if (c == '(') lexerAddLexeme(LEXEME_LEFT_BRACKET, 0);
-        else if (c == ')') lexerAddLexeme(LEXEME_RIGHT_BRACKET, 0);
+        if (c == '+') lexerAddToken(TOKEN_PLUS, 0);
+        else if (c == '-') lexerAddToken(TOKEN_MINUS, 0);
+        else if (c == '*') lexerAddToken(TOKEN_STAR, 0);
+        else if (c == '/') lexerAddToken(TOKEN_SLASH, 0);
+        else if (c == '(') lexerAddToken(TOKEN_LEFT_BRACKET, 0);
+        else if (c == ')') lexerAddToken(TOKEN_RIGHT_BRACKET, 0);
         else if (c >= '0' && c <= '9') {
             int64_t number = 0;
 
@@ -80,7 +89,7 @@ void lexer(char *code) {
                 c = code[++lexerCurrent];
             }
 
-            lexerAddLexeme(LEXEME_NUMBER, number);
+            lexerAddToken(TOKEN_NUMBER, number);
             continue;
         } else {
             printf("Unknown char: %c\n", c);
@@ -90,31 +99,269 @@ void lexer(char *code) {
     }
 }
 
+/*
+struct Node {
+    int8_t type      = NODE_LITERAL | NODE_BINOP;                                  0
+    int8_t binopType = BINOP_PLUS | BINOP_MINUS | BINOP_MULTIPLY | BINOP_DIVIDE;   1
+    int64_t literal  = NUMBER;                                                     2
+    int64_t left    = pointer to Node;                                             10
+    int64_t right   = pointer to Node;                                             18
+}
+*/
+
+int8_t *createNode() {
+    int8_t *node = malloc(26);
+    memset(node, 0, 26);
+    return node;
+}
+
+void setNodeType(int8_t *node, int8_t type) {
+    node[0] = type;
+}
+
+void setNodeBinopType(int8_t *node, int8_t binopType) {
+    node[1] = binopType;
+}
+
+void setNodeLiteral(int8_t *node, int64_t literal) {
+    memcpy(node + 2, &literal, 8);
+}
+
+void setNodeLeft(int8_t *node, int8_t *left) {
+    intptr_t addr = (intptr_t)left;
+    memcpy(node + 10, &addr, 8);
+}
+
+void setNodeRight(int8_t *node, int8_t *right) {
+    intptr_t addr = (intptr_t)right;
+    memcpy(node + 18, &addr, 8);
+}
+
+int64_t tokenGetLiteral(int8_t *token) {
+    int64_t literal;
+    memcpy(&literal, token + 1, 8);
+    return literal;
+}
+
+int parserCurrent = 0;
+
+char *formatType(int8_t type) {
+    return type == TOKEN_PLUS            ? "PLUS"
+           : type == TOKEN_MINUS         ? "MINUS"
+           : type == TOKEN_STAR          ? "STAR"
+           : type == TOKEN_SLASH         ? "SLASH"
+           : type == TOKEN_LEFT_BRACKET  ? "LEFT_BRACKET"
+           : type == TOKEN_RIGHT_BRACKET ? "RIGHT_BRACKET"
+           : type == TOKEN_NUMBER        ? "NUMBER"
+                                         : "EOF";
+}
+
+char *formatBinopType(int8_t type) {
+    return type == BINOP_PLUS       ? "+"
+           : type == BINOP_MINUS    ? "-"
+           : type == BINOP_MULTIPLY ? "*"
+                                    : "/";
+}
+
+int8_t *peek() {
+    return (int8_t *)tokens.data[parserCurrent];
+}
+
+bool isAtEnd() {
+    return *peek() == TOKEN_EOF;
+}
+
+int8_t *previous() {
+    assert(parserCurrent > 0 && "Previous at start is bad");
+
+    return (int8_t *)tokens.data[parserCurrent - 1];
+}
+
+bool check(int8_t type) {
+    if (isAtEnd())
+        return false;
+
+    int8_t *token = peek();
+    return token != 0 && *token == type;
+}
+
+int8_t *advance() {
+    if (!isAtEnd())
+        parserCurrent++;
+    return previous();
+}
+
+int8_t *consume(int8_t type) {
+    if (check(type))
+        return advance();
+
+    printf("Expected: %s, got: %s\n", formatType(type), formatType(*peek()));
+    exit(1);
+    return 0;
+}
+
+bool match(int8_t type) {
+    if (check(type)) {
+        advance();
+        return true;
+    }
+    return false;
+}
+
+int8_t *factor();
+int8_t *term();
+int8_t *expression();
+
+int8_t *factor() {
+    if (match(TOKEN_LEFT_BRACKET)) {
+        int8_t *expr = expression();
+        consume(TOKEN_RIGHT_BRACKET);
+        return expr;
+    }
+
+    int8_t *numberToken = consume(TOKEN_NUMBER);
+    int64_t literal = tokenGetLiteral(numberToken);
+
+    int8_t *node = createNode();
+    setNodeType(node, NODE_LITERAL);
+    setNodeLiteral(node, literal);
+
+    return node;
+}
+
+int8_t *term() {
+    int8_t *node = factor();
+
+    while (match(TOKEN_STAR) || match(TOKEN_SLASH)) {
+        int8_t operator = *previous();
+        int8_t *right = factor();
+        int8_t *left = node;
+
+        node = createNode();
+        setNodeType(node, NODE_BINOP);
+        setNodeBinopType(node, operator == TOKEN_STAR ? BINOP_MULTIPLY : BINOP_DIVIDE);
+        setNodeLeft(node, left);
+        setNodeRight(node, right);
+    }
+
+    return node;
+}
+
+int8_t *expression() {
+    int8_t *node = term();
+
+    while (match(TOKEN_PLUS) || match(TOKEN_MINUS)) {
+        int8_t operator = *previous();
+        int8_t *right = term();
+        int8_t *left = node;
+
+        node = createNode();
+        setNodeType(node, NODE_BINOP);
+        setNodeBinopType(node, operator == TOKEN_PLUS ? BINOP_PLUS : BINOP_MINUS);
+        setNodeLeft(node, left);
+        setNodeRight(node, right);
+    }
+
+    return node;
+}
+
+int8_t *parser() {
+    int8_t *expr = expression();
+
+    if (!isAtEnd()) {
+        printf("Expected: EOF, got: %s\n", formatType(*peek()));
+    }
+
+    return expr;
+}
+
+char *formatAst(int8_t *ast) {
+    int8_t type = *ast;
+    int8_t binopType = *(ast + 1);
+
+    if (type == NODE_LITERAL) {
+        int64_t literal = *(int64_t *)(ast + 2);
+        char *buffer = malloc(256);
+        sprintf(buffer, "%ld", literal);
+        return buffer;
+    }
+
+    int64_t leftAddr, rightAddr;
+    memcpy(&leftAddr, ast + 10, 8);
+    memcpy(&rightAddr, ast + 18, 8);
+
+    int8_t *left = (int8_t *)(intptr_t)leftAddr;
+    int8_t *right = (int8_t *)(intptr_t)rightAddr;
+
+    char *leftFormatted = formatAst(left);
+    char *rightFormatted = formatAst(right);
+
+    char *buffer = malloc(256);
+    sprintf(buffer, "(%s %s %s)", leftFormatted, formatBinopType(binopType), rightFormatted);
+    return buffer;
+}
+
+int64_t calc(int64_t left, int64_t right, int8_t operator) {
+    if (operator == BINOP_PLUS)
+        return left + right;
+    if (operator == BINOP_MINUS)
+        return left - right;
+    if (operator == BINOP_MULTIPLY)
+        return left * right;
+    if (operator == BINOP_DIVIDE) {
+        if (right == 0) printf("sorry bud, no dividing by 0\n");
+        else return left / right;
+    }
+
+    return -1;
+}
+
+int64_t interpreter(int8_t *ast) {
+    int8_t type = *ast;
+    int8_t binopType = *(ast + 1);
+
+    if (type == NODE_LITERAL) {
+        int64_t literal = *(int64_t *)(ast + 2);
+        return literal;
+    }
+
+    int64_t leftAddr, rightAddr;
+    memcpy(&leftAddr, ast + 10, 8);
+    memcpy(&rightAddr, ast + 18, 8);
+
+    int8_t *left = (int8_t *)(intptr_t)leftAddr;
+    int8_t *right = (int8_t *)(intptr_t)rightAddr;
+
+    int64_t leftValue = interpreter(left);
+    int64_t rightValue = interpreter(right);
+
+    int64_t result = calc(leftValue, rightValue, binopType);
+    return result;
+}
+
+void reset() {
+    vecClear(&tokens);
+    lexerCurrent = 0;
+    parserCurrent = 0;
+}
+
 int main() {
-    // char code[256];
+    char input[256];
 
-    // printf("> ");
-    // fgets(code, sizeof(code), stdin);
+    while (1) {
+        reset();
 
-    char code[256] = "(24+3)*6-7";
+        printf("> ");
+        fgets(input, sizeof(input), stdin);
 
-    lexer(code);
+        if (strcmp(input, "q\n") == 0) {
+            break;
+        }
 
-    for (int i = 0; i < lexemes.size; i++) {
-        int8_t *lexeme = (int8_t *)lexemes.data[i];
-        int8_t type = *lexeme;
-        int64_t literal;
-        memcpy(&literal, lexeme + 1, sizeof(int64_t));
+        lexer(input);
 
-        printf(
-            "type: %s, literal: %ld\n",
-            type == LEXEME_PLUS            ? "PLUS         "
-            : type == LEXEME_MINUS         ? "MINUS        "
-            : type == LEXEME_STAR          ? "STAR         "
-            : type == LEXEME_SLASH         ? "SLASH        "
-            : type == LEXEME_LEFT_BRACKET  ? "LEFT_BRACKET "
-            : type == LEXEME_RIGHT_BRACKET ? "RIGHT_BRACKET"
-                                           : "NUMBER       ",
-            literal);
+        int8_t *ast = parser();
+
+        printf("%ld\n", interpreter(ast));
     }
 }
